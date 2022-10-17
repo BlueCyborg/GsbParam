135ca483464ae99668e7035b73352b3e79022b21
BlueCyborg <mathisferre@gmail.com>
2022-10-17 10:07:28 +0200

    Ajout de la quantite

--- vues/v_panier.php
+++ vues/v_panier.php
@@ -1,40 +1,48 @@
 <div>
 	<img src="images/panier.gif" alt="Panier" title="panier" />
 </div>
-<div id="produits">
-	<?php
-	foreach ($lesProduitsDuPanier as $unProduit) {
-		// récupération des données d'un produit
-		$id = $unProduit['id'];
-		$description = $unProduit['description'];
-		$image = $unProduit['image'];
-		$prix = $unProduit['prix'];
-		// affichage
-	?>
-		<div class="card">
-			<div class="photoCard"><img src="<?php echo $image ?>" alt="image descriptive" /></div>
-			<div class="descrCard"><?php echo	$description; ?> </div>
-			<div class="prixCard"><?php echo $prix . "€" ?></div>
-			<div class="imgCard"><a href="index.php?uc=gererPanier&produit=<?php echo $id ?>&action=supprimerUnProduit" onclick="return confirm('Voulez-vous vraiment retirer cet article ?');">
-					<img src="images/retirerpanier.png" TITLE="Retirer du panier" alt="retirer du panier"></a>
+
+<form method="POST" action="index.php?uc=gererPanier&action=passerCommande" style="display: flex; row-gap: 2rem; align-items: center; flex-direction: column;">
+	<div id="produits">
+		<?php
+		foreach ($lesProduitsDuPanier as $unProduit) {
+			// récupération des données d'un produit
+			$id = htmlspecialchars($unProduit['id']);
+			$description = htmlspecialchars($unProduit['description']);
+			$image = htmlspecialchars($unProduit['image']);
+			$prix = htmlspecialchars($unProduit['prix']);
+			// affichage
+		?>
+
+			<div class="card">
+
+				<div class="photoCard">
+					<img src="<?php echo $image ?>" alt="image descriptive" />
+				</div>
+				<div class="descrCard"><?php echo	$description; ?> </div>
+				<div class="prixCard"><?php echo $prix . "€" ?></div>
+				<div class="imgCard">
+					<a href="index.php?uc=gererPanier&produit=<?php echo $id ?>&action=supprimerUnProduit" onclick="return confirm('Voulez-vous vraiment retirer cet article ?');">
+						<img src="images/retirerpanier.png" TITLE="Retirer du panier" alt="retirer du panier">
+					</a>
+				</div>
+				<br>
+				<br>
+				<br>
+				<br>
+				<p>Quantité
+					<input type="number" name="qte[<?= $id ?>]" min="1" value="1" required />
+				</p>
 			</div>
-			<br>
-			<br>
-			<br>
-			<br>
-			<form method="post" id="form">
-				<p>Quantité<input type="number" min="1" required></p>
-			</form>
-		</div>
-
-
-	<?php
-	}
-	?>
-	<div class="commande">
-		<a href="index.php?uc=gererPanier&action=passerCommande"><img src="images/commander.jpg" title="Passer commande" alt="Commander"></a>
+		<?php
+		}
+		?>
 	</div>
-	<div class="commande">
-		<a href="index.php?uc=gererPanier&action=viderPanier"><img height=100 src="images/vider-panier.jpg" title="Vider Panier" alt="Vider"></a>
+	<div>
+		<a style="text-decoration: none;" href="index.php?uc=gererPanier&action=viderPanier" onclick="return confirm('Voulez-vous vraiment vider le panier ?');">
+			<button type="button">Vider panier</button>
+		</a>
+		&nbsp;
+		<button type="submit">Passer commande</button>
 	</div>
-	<br />
+</form>
