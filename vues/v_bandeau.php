bea6288df31baa4ab2645ce8e0dfb65b5c77dd0e
Mathys FERRE <ferre@infogestion.local>
2023-03-22 17:29:30 +0100

    Modification du bandeau

--- vues/v_bandeau.php
+++ vues/v_bandeau.php
@@ -28,3 +28,30 @@
 		<li><a href="index.php?uc=connexion&action=connexion"> Connexion </a></li>
 	<?php } ?>
 </ul>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+
+	<img class="navbar-brand" src="images/gsb_logo.png" alt="GsbLogo" title="GsbLogo" width="100px" />
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php?uc=accueil">Accueil<span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index.php?uc=voirProduits&action=voirCategories">Nos produits par catégorie</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index.php?uc=voirProduits&action=nosProduits">Nos produits</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="index.php?uc=gererPanier&action=voirPanier">Voir son panier</a>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="window.location='index.php?uc=connexion&action=connexion'">Connexion</button>
+			<button class="btn btn-success my-2 my-sm-0" type="button" onclick="window.location='index.php?uc=connexion&action=inscription'">Inscription</button>
+		</form>
+	</div>
+</nav>
+<?php var_dump($_SESSION); ?>
